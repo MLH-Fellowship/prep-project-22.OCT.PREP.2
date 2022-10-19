95d9e7382ac4e79a66c5b44bab2dd86254ef38e2
fernanda de leon <fernanda.-deleon@hotmail.com>
2022-10-19 16:02:14 -0500

    Add dropdown menu with geo location

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { useEffect, useState } from "react";
 import './App.css';
 import logo from './mlh-prep.png'
+import Search from './search/search';
 
 function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [city, setCity] = useState("New York City")
   const [results, setResults] = useState(null);
+
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  }
+
+  
 
   useEffect(() => {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&appid=" + process.env.REACT_APP_APIKEY)
@@ -34,6 +41,9 @@ function App() {
       <img className="logo" src={logo} alt="MLH Prep Logo"></img>
       <div>
         <h2>Enter a city below 👇</h2>
+        <div className="container">
+          <Search onSearchChange={handleOnSearchChange} />
+          </div>
         <input
           type="text"
           value={city}
